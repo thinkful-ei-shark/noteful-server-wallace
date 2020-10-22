25547d54d22b7113889d9031191a847606c1ca00
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2020-10-22 00:08:03 -0400

    boilerplate created

---
+++ test/app.spec.js
@@ -0,0 +1,10 @@
+const app = require('../src/app');
+const supertest = require('supertest');
+
+describe('App', () => {
+    it('GET / responds with 200 containing "Hello, world!"', () => {
+        return supertest(app)
+            .get('/')
+            .expect(200, 'Hello, boilerplate!');
+    });
+});
