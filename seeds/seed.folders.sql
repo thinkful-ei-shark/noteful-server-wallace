5f2e5241e13dd2f6908c63a7ac9e30cb88188f20
twallace1015@gmail.com <68206140+TomWallaceJr@users.noreply.github.com>
2020-12-06 22:49:52 -0500

    finally working on server

---
+++ seeds/seed.folders.sql
@@ -0,0 +1,6 @@
+INSERT INTO folders (folder_id, folder_name, folder_number)
+VALUES
+    (1, 'Cool Folder', 1)
+    (2, 'Dumb Folder', 2),
+    (3, 'Smart Stuff', 3),
+    (4, 'Ethereum', 4);
---
+++ seeds/seed.notes.sql
@@ -0,0 +1,14 @@
+INSERT INTO notes (note_id, note_title, note_date, note_description, folder_id)
+VALUES
+    (1, 'Note Title', now(), 'A really cool note', 1),
+    (2, 'Worms', now(), 'Crawly', 2),
+    (3, 'Birds', now(), 'They fly', 1),
+    (4, 'Blockchain', now(), 'I love blockchain', 4),
+    (5, 'Football', now(), 'NFL', 3),
+    (6, 'Airplanes', now(), 'they fly', 3),
+    (7, 'Apple', now(), 'they make phones', 1),
+    (8, 'Beans', now(), 'They make you fart', 2),
+    (9, 'Jury duty', now(), 'A tradition', 3),
+    (10, 'NHL hockey', now(), 'get paid to fight on skates', 1),
+    (11, 'Mac', now(), 'Apple product', 3),
+    (12, 'Dumb note', now(), 'This not is dumb', 2);
